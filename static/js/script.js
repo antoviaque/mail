3eeb60d1a3e8dc3d2b33f89a2f0bc93a56fd1500
Xavier Antoviaque <xavier@antoviaque.org>
2012-12-16 00:12:46 +0100

    Design integration - Static files & CSS

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+/* this is all place holder for examples showing basic action */
+$(document).ready(function(){
+
+	$(".do-reply, .do-reply-all").click(function () {
+		$("#reply-wrap").toggle();
+	});
+
+});
